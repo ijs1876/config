9d787dbf12a3d7d99d922fc8ed710120d2966a9f
i1876 <i1876@ijs>
2022-05-27 15:26:18 +0900

    jjj

--- config-1/src/main/java/com/springcloud/Config1Application.java
+++ config-1/src/main/java/com/springcloud/Config1Application.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class Config1Application {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Config1Application.class, args);
 	}
