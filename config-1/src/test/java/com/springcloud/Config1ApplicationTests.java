ddde6bc1eb1874049e57dead6f3ac97c4cf9b0ad
i1876 <i1876@ijs>
2022-05-27 15:28:43 +0900

    init

---
+++ config-1/src/test/java/com/springcloud/Config1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Config1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
