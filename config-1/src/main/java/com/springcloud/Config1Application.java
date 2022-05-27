cdef86fe8a337e36fa138cd568e70851d4e2afde
i1876 <i1876@ijs>
2022-05-27 15:15:27 +0900

    dddd

---
+++ config-1/src/main/java/com/springcloud/Config1Application.java
@@ -0,0 +1,16 @@
+package com.springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Config1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Config1Application.class, args);
+	}
+
+}
